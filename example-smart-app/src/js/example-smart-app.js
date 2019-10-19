e042b67718ff2b6a7f671960158ab407d3d8d047
zz7 <zz7@users.noreply.github.com>
2019-10-19 15:01:45 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -16,7 +16,7 @@
                     query: {
                       code: {
                         $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
-                              'http://loinc.org|3141-9', 'http://loinc.org|3141-9',
+                              'http://loinc.org|29463-7', 'http://loinc.org|29463-7',
                               'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
                               'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
                       }
@@ -38,7 +38,7 @@
           }
 
           var height = byCodes('8302-2');
-          var weight = byCodes('3141-9');
+          var weight = byCodes('29463-7');
           var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
